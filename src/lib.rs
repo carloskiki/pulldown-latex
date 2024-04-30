f7812c9d0446b1880e391ef2b68b3545def09a50
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2024-04-30 15:47:18 -0400

    re-export useful stuff

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,7 @@ pub mod config;
 pub mod event;
 pub mod mathml;
 pub mod parser;
+
+pub use parser::{Parser, ParserError};
+pub use config::RenderConfig;
+pub use mathml::{push_mathml, write_mathml};
