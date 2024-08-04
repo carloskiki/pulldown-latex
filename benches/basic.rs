8929103578cc66d6a581490247f231f030782833
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2024-08-04 13:04:41 -0400

    move benchmarks to criterion to not rely on nightly for them

--- benches/basic.rs
+++ benches/basic.rs
@@ -1,14 +1,8 @@
-#![feature(test)]
-
-extern crate test;
-
-use test::Bencher;
-
+use criterion::{criterion_group, criterion_main, Criterion};
 use pulldown_latex::parser::Parser;
 
-#[bench]
-fn match_on_greek(b: &mut Bencher) {
-    b.iter(|| {
+fn match_on_greek(c: &mut Criterion) {
+    c.bench_function("match on greek", |b| b.iter(|| {
     let storage = pulldown_latex::Storage::new();
     let parser = Parser::new(r"
 \alpha \beta \gamma \delta \epsilon \zeta \eta \theta
@@ -34,36 +28,37 @@ fn match_on_greek(b: &mut Bencher) {
 \Iota \Kappa \Lambda \Mu \Nu \Xi \Omicron \Pi
 ", &storage);
         let mut str = String::new();
-        test::black_box(pulldown_latex::mathml::push_mathml(&mut str, parser, Default::default()).unwrap());
+        pulldown_latex::mathml::push_mathml(&mut str, parser, Default::default()).unwrap();
+    }));
+}
+
+fn subscript_torture(c: &mut Criterion) {
+    c.bench_function("subscript torture", |b| {
+        b.iter(|| {
+            let storage = pulldown_latex::Storage::new();
+            let parser = Parser::new("a_{5_{5_{5_{5_{5_{5_{5_{5_{5_{5_{5_5}}}}}}}}}}}", &storage);
+            let mut str = String::new();
+            pulldown_latex::mathml::push_mathml(&mut str, parser, Default::default()).unwrap();
+        })
     });
 }
 
-#[bench]
-fn subscript_torture(b: &mut Bencher) {
-    b.iter(|| {
-        let storage = pulldown_latex::Storage::new();
-        let parser = Parser::new("a_{5_{5_{5_{5_{5_{5_{5_{5_{5_{5_{5_5}}}}}}}}}}}", &storage);
-        let mut str = String::new();
-        test::black_box(
-            pulldown_latex::mathml::push_mathml(&mut str, parser, Default::default()).unwrap(),
-        );
-    });
-}
-
-#[bench]
-fn basic_macros(b: &mut Bencher) {
-    b.iter(|| {
-        let storage = pulldown_latex::Storage::new();
-        let parser = Parser::new(
-            r"\def\d{\mathrm{d}}
+fn basic_macro(c: &mut Criterion) {
+    c.bench_function("basic macro", |b| {
+        b.iter(|| {
+            let storage = pulldown_latex::Storage::new();
+            let parser = Parser::new(
+                r"\def\d{\mathrm{d}}
                 \oint_C \vec{B}\circ \d\vec{l} = \mu_0 \left( I_{\text{enc}}
                 + \varepsilon_0 \frac{\d}{\d t} \int_S {\vec{E} \circ \hat{n}}\;
                 \d a \right)",
-            &storage,
-        );
-        let mut str = String::new();
-        test::black_box(
-            pulldown_latex::mathml::push_mathml(&mut str, parser, Default::default()).unwrap(),
-        );
+                &storage,
+            );
+            let mut str = String::new();
+            pulldown_latex::mathml::push_mathml(&mut str, parser, Default::default()).unwrap();
+        })
     });
 }
+
+criterion_group!(benches, basic_macro, match_on_greek, subscript_torture);
+criterion_main!(benches);
