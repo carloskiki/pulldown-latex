d665c8f1d506c11938aac42daad54564e265d2ad
Charles Edward Gagnon <76854355+carloskiki@users.noreply.github.com>
2026-06-02 21:31:01 -0400

    working cross-browser tests

--- tests/cross-browser.rs
+++ tests/cross-browser.rs
@@ -1,15 +1,20 @@
-use std::path::Path;
+use std::{io::Write, path::Path, process::Stdio, time::Duration};
 
-use common::{cross_browser, cross_browser_tabled, html_template, OUTPUT_DIR};
+use common::{html_template, OUTPUT_DIR};
+use fantoccini::{ClientBuilder, Locator};
+use heck::ToTitleCase;
 use pulldown_latex::config::DisplayMode;
+use tokio::process::Command;
+
+use crate::common::RENDERED;
 
 mod common;
 
 fn main() {
     let concl = common::test();
-    if std::env::var("RENDER").as_deref() != Ok("true") {
-        concl.exit()
-    }
+    // if std::env::var("RENDER").as_deref() != Ok("true") {
+    //     concl.exit()
+    // }
 
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(cross_browser()).unwrap();
@@ -23,6 +28,114 @@ fn main() {
     )
     .unwrap();
     concl.exit()
+}
+
+pub async fn cross_browser() -> anyhow::Result<()> {
+    let chromedriver = driver_command("chromedriver", "CHROMEDRIVER");
+    browser_tests(&chromedriver, "chrome").await?;
+    let geckodriver = driver_command("geckodriver", "GECKODRIVER");
+    browser_tests(&geckodriver, "firefox").await?;
+
+    #[cfg(target_os = "macos")]
+    {
+        let safaridriver = driver_command("safaridriver", "SAFARIDRIVER");
+        browser_tests(&safaridriver, "safari").await?
+    }
+
+    Ok(())
+}
+
+fn driver_command(default_command: &str, env_var: &str) -> String {
+    std::env::var(env_var)
+        .or_else(|_| std::env::var(format!("{env_var}_CMD")))
+        .ok()
+        .filter(|value| !value.trim().is_empty())
+        .unwrap_or_else(|| default_command.to_string())
+}
+
+async fn browser_tests(command: &str, name: &str) -> anyhow::Result<()> {
+    let mut tmp = tempfile::Builder::new().suffix(".html").tempfile()?;
+    let path = format!("file://{}", tmp.path().to_str().unwrap());
+
+    let mut process = Command::new(command)
+        .arg("--port=4444")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .kill_on_drop(true)
+        .spawn()?;
+    std::thread::sleep(std::time::Duration::from_millis(1000));
+    let client = ClientBuilder::native()
+        .connect("http://localhost:4444")
+        .await?;
+
+    let rendered = &*RENDERED.lock().unwrap();
+    for (table_name, rows) in rendered {
+        for (_input, output) in rows {
+            html_template(
+                tmp.as_file_mut(),
+                "",
+                Some("cross-browser-render.css"),
+                |file: &mut std::fs::File| -> anyhow::Result<()> {
+                    file.write_all(output.as_bytes())?;
+                    Ok(())
+                },
+            )?;
+
+            client.goto(&path).await?;
+            let elem = client
+                .wait()
+                .at_most(Duration::from_secs(10))
+                .for_element(Locator::XPath("/html/body"))
+                .await?;
+
+            let screenshot = elem.screenshot().await?;
+
+            tokio::fs::write(
+                format!("{OUTPUT_DIR}/screenshots/{name}/{table_name}.png"),
+                screenshot,
+            )
+            .await?;
+            tmp.as_file().set_len(0)?;
+        }
+    }
+    tmp.close()?;
+    client.close().await?;
+    process.kill().await?;
+
+    Ok(())
+}
+
+pub fn cross_browser_tabled(file: &mut std::fs::File) -> anyhow::Result<()> {
+    file.write_all(br#"<table style="margin: auto;">"#)?;
+    file.write_all(br#"<tr><th>Input</th><th>Chrome</th><th>Firefox</th><th>Safari</th></tr>"#)?;
+    let mut rendered = RENDERED.lock().unwrap();
+    rendered.sort();
+    rendered
+        .iter()
+        .try_for_each(|(table_name, rows)| -> std::io::Result<()> {
+            file.write_fmt(format_args!(
+                r#"<tr><th colspan="4">{table_name}</th></tr>"#,
+                table_name = table_name.to_title_case()
+            ))?;
+
+            file.write_all(br#"<tr><td class="input">"#)?;
+            rows.iter()
+                .try_for_each(|(input, _)| -> std::io::Result<()> {
+                    file.write_all(input.as_bytes())?;
+                    file.write_all(b"\n")?;
+                    Ok(())
+                })?;
+            file.write_all(b"</td>")?;
+            for browser in ["chrome", "firefox", "safari"] {
+                file.write_fmt(
+                    format_args!(r#"<td class="image-container"><img class="{browser}-img" src="{OUTPUT_DIR}/screenshots/{browser}/{table_name}.png"></td>"#)
+                )?;
+            }
+            file.write_all(b"</tr>")?;
+            Ok(())
+        })?;
+    file.write_all(b"</table>")?;
+    Ok(())
 }
 
 round_trip! {basic, "5 + 5 = 10", display_mode = DisplayMode::Block}
